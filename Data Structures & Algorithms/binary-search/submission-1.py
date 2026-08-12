f0b5f2b1ce791ddfc8372809e28b4bfb214e1488
Miguel Castrejon <35345857+mcastre1@users.noreply.github.com>
2026-08-12 15:12:07 -0600

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        window = (0, len(nums))
+        found = False
+
+        while not found:
+            if window[1] - window[0] <= 1:
+                print(window)
+                found = True
+            
+            mid = int((window[1] - window[0]) / 2)
+            
+            if target >= nums[window[0] + mid]:
+                window = (window[0] + mid, window[1])
+            else:
+                window = (window[0], window[1] - mid)
+
+        if nums[window[0]] == target:
+            return window[0]
+        else:
+            return -1
+            
